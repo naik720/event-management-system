9fc9f34fe2bc77827cace47c0f2b3923297f3599
Keerti12-ui <keertitbhat12@gmail.com>
2026-05-19 14:52:59 +0530

    Added user register files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      {/* Default route redirects to the login page */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      {/* Route for Login page */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Route for Register page */}
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
